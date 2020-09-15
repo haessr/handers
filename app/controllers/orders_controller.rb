9eafe5b7032b4fefe178c6ddd1d3e22115320adb
Rafa Ruigomez <rafaruigomez@gmail.com>
2020-09-15 17:25:59 +0200

    Added pundit &Authorize

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,27 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.all
+    # @orders = Order.all
+    @orders = policy_scope(Reservation).includes(:flat)
   end
 
   def show
     @order = Order.find(params[:id])
+    authorize @order
   end
 
   def create
 
     @order = Order.new(order_params)
     @order.user = current_user
+    authorize @order
 
     if @order.save
       redirect_to order_path(@order)
     else
       redirect_to product_path(@order.product)
     end
+
   end
 
   private
