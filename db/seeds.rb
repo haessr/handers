9b170a825288afb221af1cfaa090a4120a099c9e
Haess Romani <hromaza@gmail.com>
2020-09-21 22:54:05 -0500

    new seed to clean all db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 require 'faker'
 
 puts "Cleaning database..."
 # User.destroy_all
 
+puts "Cleaning Messages Table..."
+Message.destroy_all
 
+puts "Cleaning Chatrooms Table..."
+Chatroom.destroy_all
 
+puts "Cleaning Orders Table..."
+Order.destroy_all
+
+puts "Cleaning Products Table..."
+Product.destroy_all
+
+puts "Cleaning NGOs Table..."
+Npo.destroy_all
+
+puts "Creating NGO"
 Npo.create(name: "Greenpeace",url: "www.greenpeace.org", url_photo: "https://storage.googleapis.com/planet4-colombia-stateless/2020/05/65f24819-logogreenpeace_web.jpg", description: "Greenpeace is a non-governmental environmental organization with offices in over 55 countries and an international coordinating body in Amsterdam, the Netherlands. Greenpeace was founded in 1971 by Irving Stowe and Dorothy Stowe, Canadian and US ex-pat environmental activists.")
 Npo.create(name: "UNICEF", url: "www.unicef.org", url_photo: "https://www.guiaongs.org/wp-content/uploads/2015/09/unicef_destacada.png", description: "UNICEF, or the United Nations Children's Fund[a] is a United Nations agency responsible for providing humanitarian and developmental aid to children worldwide. U.N. headquarters is based in New York City, it is among the most widespread and recognizable social welfare organizations in the world, with a presence in 192 countries and territories.")
 Npo.create(name: "WWF", url: "wwf.panda.org", url_photo: "https://d2ouvy59p0dg6k.cloudfront.net/img/original/wwflogo_apcolor_01_copy.png", description: "The World Wide Fund for Nature (WWF) is an international non-governmental organization founded in 1961 that works in the field of wilderness preservation and the reduction of human impact on the environment. It was formerly named the World Wildlife Fund, which remains its official name in Canada and the United States.")
