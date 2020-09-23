03ac4bc396fb830877569c65c62ba6ee5f2b0de3
Rafa Ruigomez <rafaruigomez@gmail.com>
2020-09-23 15:40:19 +0200

    policy payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
         }],
         success_url: order_url(@order),
         cancel_url: order_url(@order)
-      )    
+      )
       @order.update(checkout_session_id: session.id)
       redirect_to new_order_payment_path(@order)
       # redirect_to order_path(@order)
